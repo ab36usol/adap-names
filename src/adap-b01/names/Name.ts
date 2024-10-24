1b384b2ae850a469c9b66c5f365e7acdf58d88b6
Alexander <alexander.karenberg@fau.de>
2024-10-24 14:13:23 +0200

    exercise b01 solved

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -7,37 +7,54 @@ export class Name {
     private delimiter: string = this.DEFAULT_DELIMITER;
 
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation");
+        this.components = other
+        this.delimiter = (delimiter !== undefined && delimiter !== null) ? delimiter: this.delimiter
+        //throw new Error("needs implementation");
     }
 
     /** Returns human-readable representation of Name instance */
     public asNameString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        let nameString: string = ""
+        for(let i=0;i<this.components.length;i++) {
+            nameString += this.components[i]
+            if(i<this.components.length-1) 
+                nameString += this.delimiter
+        }
+        return nameString
+        //throw new Error("needs implementation");
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation");
+        if (i === undefined || null) 
+            throw new Error("i is undefined or null");
+        return this.components[i]
+        //throw new Error("needs implementation");
     }
 
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation");
+        this.components[i] = c
+        //throw new Error("needs implementation");
     }
 
-     /** Returns number of components in Name instance */
-     public getNoComponents(): number {
-        throw new Error("needs implementation");
+    public getNoComponents(): number {
+        return this.components.length
+        //throw new Error("needs implementation");
     }
 
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation");
+        
+        this.components.splice(i, 0, c)
+        //throw new Error("needs implementation");
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation");
+        this.components.push(c);
+        //throw new Error("needs implementation");
     }
 
     public remove(i: number): void {
-        throw new Error("needs implementation");
+        delete this.components[i]
+        //throw new Error("needs implementation");
     }
 
 }
